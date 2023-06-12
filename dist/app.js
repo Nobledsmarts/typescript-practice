506f50163e382784f8bf5b90beae40bc5adebe9e
nobledsmarts <nobledsmarts@gmail.com>
2023-06-12 19:00:47 +0100

    typescript classes

--- dist/app.js
+++ dist/app.js
@@ -1,3 +1,20 @@
 "use strict";
-console.log('mmm');
+class Department {
+    constructor(n) {
+        this.employees = [];
+        this.name = n;
+    }
+    addEmployees(employee) {
+        this.employees.push(employee);
+        ;
+    }
+    privateEmployeeInformation() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+const accounting = new Department('Accounting');
+accounting.addEmployees('Max');
+accounting.addEmployees('Manu');
+accounting.privateEmployeeInformation();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,23 @@
-console.log('mmm');
+class Department{
+    name: string;
+    employees : string[] = [];
 
+    constructor(n: string){
+        this.name = n;
+    }
+
+    addEmployees(employee: string){
+        this.employees.push(employee);;
+    }
+    privateEmployeeInformation(){
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+}
+
+const accounting = new Department('Accounting');
+
+accounting.addEmployees('Max');
+accounting.addEmployees('Manu');
+
+accounting.privateEmployeeInformation();
