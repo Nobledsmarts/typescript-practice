ddbb9246a903e6b3d026a409b08442615315a550
nobledsmarts <nobledsmarts@gmail.com>
2023-06-13 16:52:35 +0100

    decorator samples

--- dist/app.js
+++ dist/app.js
@@ -5,15 +5,29 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(logMsg, root) {
+function Logger(logMsg) {
     return function (construct) {
-        let target = new construct();
-        console.log(target);
+        // let target = new construct();
+        // console.log(target);
+        // let m = new target()
+        console.log(logMsg);
+        // target
+    };
+}
+function WithTemplate(logMsg, root) {
+    return function (originalConstructor) {
+        // let target = new originalConstructor();
+        // console.log(target);
         // let m = new target();
-        let app = document.getElementById(root);
-        app.innerHTML = logMsg;
-        console.log('app started by ' + target.name);
         // target.name
+        return class extends originalConstructor {
+            constructor(...args) {
+                super();
+                let app = document.getElementById(root);
+                app.innerHTML = logMsg;
+                console.log('app started by ' + this.name);
+            }
+        };
     };
 }
 let Person = class Person {
@@ -23,8 +37,27 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger('Testing Logger', 'app')
+    Logger('Testing Logger'),
+    WithTemplate('<h1>Testing Logger</h1>', 'app')
 ], Person);
 let pers = new Person('noble');
 console.log(pers);
+function log(target, propertyName) {
+    console.log('property decorator');
+    console.log(target, propertyName);
+}
+class Product {
+    constructor() {
+        this.title = "";
+        this._price = 100;
+    }
+    set price(price) {
+        if (price > 0) {
+            this._price = price;
+        }
+    }
+}
+__decorate([
+    log
+], Product.prototype, "title", void 0);
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,33 @@
-function Logger(logMsg: string, root: string){
+function Logger(logMsg: string){
     return function(construct: any){
-        let target = new construct();
-        console.log(target);
-        // let m = new target();
-        let app = document.getElementById(root)!;
-        app.innerHTML = logMsg;
-
-        console.log('app started by ' + target.name);
-        // target.name
+        // let target = new construct();
+        // console.log(target);
+        // let m = new target()
+        console.log(logMsg);
+        // target
     }
 }
 
-@Logger('Testing Logger', 'app')
+function WithTemplate(logMsg: string, root: string){
+    return function<T extends {new(...args: any[]) : { name : string }}>(originalConstructor: T){
+        // let target = new originalConstructor();
+        // console.log(target);
+        // let m = new target();
+        // target.name
+        return class extends originalConstructor{
+            constructor(...args: any[]){
+                super();
+                let app = document.getElementById(root)!;
+                app.innerHTML = logMsg;
+
+                console.log('app started by ' + this.name);
+            }
+        }
+    }
+}
+
+@Logger('Testing Logger')
+@WithTemplate('<h1>Testing Logger</h1>', 'app')
 class Person{
     name: string = 'noble';
     constructor(n: string){
@@ -23,5 +39,25 @@ let pers = new Person('noble');
 
 console.log(pers);
 
+function log(target: any, propertyName: string | symbol){
+    console.log('property decorator');
+    console.log(target, propertyName);
+}
 
 
+class Product {
+    @log
+    title: string;
+    private _price: number;
+
+    constructor(){
+        this.title = "";
+        this._price = 100;
+    }
+
+    set price(price: number) {
+        if(price > 0){
+            this._price = price;
+        }
+    }
+}
