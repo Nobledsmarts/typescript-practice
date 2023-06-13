b5ae32a6ecb49f543bc29c3f6c4602cb7c332c3c
nobledsmarts <nobledsmarts@gmail.com>
2023-06-13 11:32:38 +0100

    generics

--- dist/app.js
+++ dist/app.js
@@ -7,4 +7,21 @@ function merge(objA, objB) {
 }
 const merged = merge({ name: 'mm', hobbies: ['mm'] }, { age: 80 });
 console.log(merged);
+function countAndDescribe(element) {
+    let descriptionText = "Got no value";
+    if (element.length === 1) {
+        descriptionText = "Got 1 element";
+    }
+    else if (element.length > 1) {
+        descriptionText = "Got " + element.length + ' elements';
+    }
+    return [element, descriptionText];
+}
+console.log(countAndDescribe(['Hi', 'there']));
+function extractAndConvert(obj, key) {
+    return 'Value ' + obj[key];
+}
+console.log(extractAndConvert({
+    name: 'nob'
+}, 'name'));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -11,3 +11,27 @@ function merge<A extends object, B extends object>(objA: A, objB:B){
 const merged = merge({name : 'mm', hobbies : ['mm']}, { age : 80 });
 
 console.log(merged);
+
+interface Lengthy {
+    length : number;
+}
+
+function countAndDescribe<T extends Lengthy>(element : T): [T, string]{
+    let descriptionText = "Got no value";
+    if(element.length === 1){
+        descriptionText = "Got 1 element";
+    } else if(element.length > 1){
+        descriptionText = "Got " + element.length  + ' elements';
+    }
+    return [element, descriptionText];
+}
+
+console.log(countAndDescribe(['Hi', 'there']));
+
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U){
+    return 'Value ' + obj[key];
+}
+
+console.log(extractAndConvert({
+    name : 'nob'
+}, 'name'));
